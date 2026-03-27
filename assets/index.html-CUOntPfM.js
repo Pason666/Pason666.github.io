f93a2966d3dd70b1ef1cb838ecba071957d96ee1
Pason666 <Pason666@users.noreply.github.com>
2026-03-27 01:51:51 +0000

    deploy: 1fb080a11110cf111e60e9f4c105611597a46e85

---
+++ assets/index.html-CUOntPfM.js
@@ -0,0 +1,12 @@
+import{_ as t}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as e,d as n,o}from"./app-BNyHiUKy.js";const a={};function r(m,c){return o(),e("div",null,[n(` ---
+title: Dream
+icon: cloud
+date: 2026-03-27
+category: summary
+---
+## 一些古怪的梦
+
+为什么明明是大脑幻想出来的，醒来后心却会那么难受呢
+
+---
+<Catalog /> `)])}const p=t(a,[["render",r]]),d=JSON.parse('{"path":"/posts/dream/","title":"","lang":"en-US","frontmatter":{"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2026-03-27T01:48:31.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Pason\\",\\"url\\":\\"https://github.com/Pason666\\"}]}"],["meta",{"property":"og:url","content":"https://mister-hope.github.io/posts/dream/"}],["meta",{"property":"og:site_name","content":"Blog"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2026-03-27T01:48:31.000Z"}],["meta",{"property":"article:modified_time","content":"2026-03-27T01:48:31.000Z"}]]},"git":{"createdTime":1774576111000,"updatedTime":1774576111000,"contributors":[{"name":"Pason666","username":"Pason666","email":"110419260@qq.com","commits":1,"url":"https://github.com/Pason666"}]},"readingTime":{"minutes":0.14,"words":42},"filePathRelative":"posts/dream/README.md","excerpt":"<!-- ---\\ntitle: Dream\\nicon: cloud\\ndate: 2026-03-27\\ncategory: summary\\n---\\n## 一些古怪的梦\\n\\n为什么明明是大脑幻想出来的，醒来后心却会那么难受呢\\n\\n---\\n<Catalog /> -->"}');export{p as comp,d as data};
